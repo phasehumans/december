b0917b49433c542cc97641fe5666fc46baf28eec
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-02-15 18:08:14 +0530

    db connect

--- server/db.ts
+++ server/db.ts
@@ -1,10 +1,10 @@
-// import { PrismaClient } from './generated/prisma/client'
-// import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from "./src/generated/prisma/client"; 
+import { PrismaPg } from "@prisma/adapter-pg";
 
-// const adapter = new PrismaPg({
-//   connectionString: process.env.DATABASE_URL!,
-// })
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!
+})
 
-// export const prisma = new PrismaClient({
-//   adapter,
-// })
+export const prisma = new PrismaClient({
+    adapter
+})
