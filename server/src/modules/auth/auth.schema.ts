ee3c87128bd143bf2b02c07b4e8d69d73897d1e4
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-02-16 17:46:21 +0530

    add auth signin, login and logout

--- server/src/modules/auth/auth.schema.ts
+++ server/src/modules/auth/auth.schema.ts
@@ -1,10 +1,13 @@
-import { z } from 'zod'
+import { password } from 'bun'
+import { email, z } from 'zod'
 
 export const signupSchema = z.object({
-    name: z.string().min(3).max(20),
-    email: z.string().email(),
-    password: z.string().min(6).max(20)
+  name: z.string().min(3).max(20),
+  email: z.string().email(),
+  password: z.string().min(6).max(20),
 })
 
-const loginSchema = z.object({})
-
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6).max(20),
+})
