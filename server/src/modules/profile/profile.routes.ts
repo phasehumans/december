81cae2b863dcc4f8a4cc8b5657eab5eef034b50b
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-02-16 23:41:16 +0530

    feat: add profile

--- server/src/modules/profile/profile.routes.ts
+++ server/src/modules/profile/profile.routes.ts
@@ -1,11 +1,12 @@
-import { Router } from "express";
-import { authMiddleware } from "../../middleware/auth.middleware";
+import { Router } from 'express'
+import { authMiddleware } from '../../middleware/auth.middleware'
+import { profileController } from './profile.controller'
 
 const profileRouter = Router()
 
 profileRouter.use(authMiddleware)
-profileRouter.get('/me')
-profileRouter.patch('/update-name')
-profileRouter.patch('/change-password')
+profileRouter.get('/me', profileController.getProfile)
+profileRouter.patch('/update-name', profileController.updateName)
+profileRouter.patch('/change-password', profileController.changePassword)
 
 export default profileRouter
