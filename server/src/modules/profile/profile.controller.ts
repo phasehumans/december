f73a93287ece2d834664bc6d4f0e196f737f2d5a
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-02-16 23:41:16 +0530

    feat: add profile

--- server/src/modules/profile/profile.controller.ts
+++ server/src/modules/profile/profile.controller.ts
@@ -1,34 +1,38 @@
-import type { Request, Response } from "express"
-import { profileService } from "./profile.service"
-import { success } from "zod"
+import type { Request, Response } from 'express'
+import { profileService } from './profile.service'
 
 const getProfile = async (req: Request, res: Response) => {
-    const userId = req.userId
+  const userId = req.userId
 
-    if(!userId){
-        return res.status(400).json({
-            success: false,
-            message: "unauthorized"
-        })
-    }
+  if (!userId) {
+    return res.status(400).json({
+      success: false,
+      message: 'unauthorized',
+    })
+  }
 
-    try {
-        const result = await profileService.getProfile(userId)
-        return res.status(200).json({
-            success: true,
-            message: "profile details",
-            data: result
-        })
-    } catch (error: any) {
-        return res.status(400).json({
-            success: false,
-            message: "user doesnot exist",
-            errors: error.message
-        })
-    }
+  try {
+    const result = await profileService.getProfile(userId)
+    return res.status(200).json({
+      success: true,
+      message: 'profile details',
+      data: result,
+    })
+  } catch (error: any) {
+    return res.status(400).json({
+      success: false,
+      message: 'user doesnot exist',
+      errors: error.message,
+    })
+  }
 }
 
+const updateName = async (req: Request, res: Response) => {}
+
+const changePassword = async (req: Request, res: Response) => {}
 
 export const profileController = {
-    getProfile,
+  getProfile,
+  updateName,
+  changePassword,
 }
--- server/src/modules/profile/profile.service.ts
+++ server/src/modules/profile/profile.service.ts
@@ -1,27 +1,43 @@
-import { email } from "zod"
-import { prisma } from "../../utils/db"
+import { email } from 'zod'
+import { prisma } from '../../utils/db'
 
 type getProfileType = {
-    userId: string
+  userId: string
+}
+
+type updateNameType = {
+  userId: string
+  name: string
+}
+
+type changePasswordType = {
+  userId: string
+  password: string
 }
 
 const getProfile = async (data: getProfileType) => {
-    const profile = await prisma.user.findUnique({
-        where: {
-            id: data.userId
-        }
-    })
+  const profile = await prisma.user.findUnique({
+    where: {
+      id: data.userId,
+    },
+  })
 
-    if(!profile){
-        throw new Error("user doesnot exist")
-    }
+  if (!profile) {
+    throw new Error('user doesnot exist')
+  }
 
-    return {
-        name: profile.name,
-        email: profile.email,
-    }
+  return {
+    name: profile.name,
+    email: profile.email,
+  }
 }
 
+const updateName = async (data: updateNameType) => {}
+
+const changePassword = async (data: changePasswordType) => {}
+
 export const profileService = {
-    getProfile
+  getProfile,
+  updateName,
+  changePassword,
 }
