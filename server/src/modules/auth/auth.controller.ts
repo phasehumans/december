0199a6e4dded97ce351d90a1c05716d38647a672
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-02-16 16:44:42 +0530

    signup controller

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,29 +1,40 @@
+import { signupSchema } from "./auth.schema"
+import type { Request, Response } from "express"
+import { authService } from "./auth.service"
+import { success } from "zod"
 
-const signup = async () => {
+const signup = async (req: Request, res: Response) => {
+  const parseData = signupSchema.safeParse(req.body)
 
+  if(!parseData.success){
+    return res.status(400).json({
+      success: false,
+      message: "invalid inputs",
+      errors: parseData.error.flatten()
+    })
+  }
+
+  try {
+    const result = await authService.signup(parseData.data)
+    return res.status(201).json({
+      success: true,
+      message: "user created successfully",
+      data: result
+    })
+  } catch (error: any) {
+    return res.status(409).json({
+      success: false,
+      errors: error.message
+    })
+  }
 }
 
-const login = async () => {
+const login = async () => {}
 
-}
-
-const getMe = async () => {
-
-}
-
-const logout = async () => {
-
-}
-
-const changePassword = async () => {
-
-}
-
+const logout = async () => {}
 
 export const authController = {
-    signup,
-    login,
-    getMe,
-    logout,
-    changePassword
+  signup,
+  login,
+  logout,
 }
--- server/src/modules/auth/auth.routes.ts
+++ server/src/modules/auth/auth.routes.ts
@@ -1,14 +1,10 @@
-import { Router } from "express";
-import { authController } from "./auth.controller";
+import { Router } from 'express'
+import { authController } from './auth.controller'
 
 const authRouter = Router()
 
 authRouter.post('/signup', authController.signup)
 authRouter.post('/login', authController.login)
-
-authRouter.get('/me', authController.getMe)
 authRouter.post('/logout', authController.logout)
-authRouter.patch('/change-password', authController.changePassword)
-
 
 export default authRouter
