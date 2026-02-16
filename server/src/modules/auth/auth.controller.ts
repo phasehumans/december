dc7246403f5fe893b009d5a07893f7ff02378c71
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-02-16 17:46:21 +0530

    add auth signin, login and logout

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,16 +1,16 @@
-import { signupSchema } from "./auth.schema"
-import type { Request, Response } from "express"
-import { authService } from "./auth.service"
-import { success } from "zod"
+import { loginSchema, signupSchema } from './auth.schema'
+import type { Request, Response } from 'express'
+import { authService } from './auth.service'
+import { success } from 'zod'
 
 const signup = async (req: Request, res: Response) => {
   const parseData = signupSchema.safeParse(req.body)
 
-  if(!parseData.success){
+  if (!parseData.success) {
     return res.status(400).json({
       success: false,
-      message: "invalid inputs",
-      errors: parseData.error.flatten()
+      message: 'invalid inputs',
+      errors: parseData.error.flatten(),
     })
   }
 
@@ -18,20 +18,61 @@ const signup = async (req: Request, res: Response) => {
     const result = await authService.signup(parseData.data)
     return res.status(201).json({
       success: true,
-      message: "user created successfully",
-      data: result
+      message: 'user created successfully',
+      data: result,
     })
   } catch (error: any) {
     return res.status(409).json({
       success: false,
-      errors: error.message
+      errors: error.message,
     })
   }
 }
 
-const login = async () => {}
+const login = async (req: Request, res: Response) => {
+  const parseData = loginSchema.safeParse(req.body)
 
-const logout = async () => {}
+  if (!parseData.success) {
+    return res.status(400).json({
+      success: false,
+      message: 'invalid inputs',
+      errors: parseData.error.flatten(),
+    })
+  }
+
+  try {
+    const result = await authService.login(parseData.data)
+    return res.status(200).json({
+      success: true,
+      message: 'user login successfully',
+      data: result,
+    })
+  } catch (error: any) {
+    return res.status(401).json({
+      success: false,
+      errors: error.message,
+    })
+  }
+}
+
+const logout = async (req: Request, res: Response) => {
+  try {
+    await authService.logout()
+
+    res.clearCookie('token')
+
+    return res.status(200).json({
+      success: true,
+      message: 'logged out successfully',
+    })
+  } catch (error: any) {
+    return res.status(500).json({
+      success: false,
+      message: 'internal server error',
+      errors: error.message,
+    })
+  }
+}
 
 export const authController = {
   signup,
