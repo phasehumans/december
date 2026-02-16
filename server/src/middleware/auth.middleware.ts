be8b54dc9277cdb536724ef55e2dcc8e01826d2c
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-02-16 22:00:29 +0530

    feat: auth middleware

---
+++ server/src/middleware/auth.middleware.ts
@@ -0,0 +1,37 @@
+import type { Request, Response, NextFunction } from "express"
+import jwt from "jsonwebtoken"
+import { success } from "zod"
+
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const token = req.headers.authorization
+    
+        if(!token || Array.isArray(token)){
+            return res.status(400).json({
+                success: false,
+                message: "unauthorized"
+            })
+        }
+    
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!)
+    
+        if (typeof decoded == "string"){
+            return res.status(401).json({
+                success: false,
+                message: "invalid token"
+            })
+        }
+        
+        req.userId = decoded.userId
+        next()
+    } catch (error: any) {
+        return res.status(500).json({
+            success: false,
+            message: "internal server error",
+            errors: error.message
+        })
+    }
+    
+
+
+}
