7db8cb020a30837a086d15d0b6528b1670f15ee4
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-02-17 22:29:11 +0530

    feat: profile

--- server/src/modules/profile/profile.service.ts
+++ server/src/modules/profile/profile.service.ts
@@ -3,94 +3,94 @@ import { prisma } from '../../utils/db'
 import bcrypt from 'bcrypt'
 
 type getProfileType = {
-  userId: string
+    userId: string
 }
 
 type updateNameType = {
-  userId: string
-  name: string
+    userId: string
+    name: string
 }
 
 type changePasswordType = {
-  userId: string
-  password: string
+    userId: string
+    password: string
 }
 
 const getProfile = async (data: getProfileType) => {
-  const profile = await prisma.user.findUnique({
-    where: {
-      id: data.userId,
-    },
-  })
+    const profile = await prisma.user.findUnique({
+        where: {
+            id: data.userId,
+        },
+    })
 
-  if (!profile) {
-    throw new Error('user doesnot exist')
-  }
+    if (!profile) {
+        throw new Error('user doesnot exist')
+    }
 
-  return {
-    name: profile.name,
-    email: profile.email,
-  }
+    return {
+        name: profile.name,
+        email: profile.email,
+    }
 }
 
 const updateName = async (data: updateNameType) => {
-  const { name, userId } = data
+    const { name, userId } = data
 
-  const existingUser = await prisma.user.findUnique({
-    where: {
-      id: userId,
-    },
-  })
+    const existingUser = await prisma.user.findUnique({
+        where: {
+            id: userId,
+        },
+    })
 
-  if (!existingUser) {
-    throw new Error('user does not exist')
-  }
+    if (!existingUser) {
+        throw new Error('user does not exist')
+    }
 
-  const updatedUser = await prisma.user.update({
-    where: {
-      id: userId,
-    },
-    data: {
-      name: name,
-    },
-  })
+    const updatedUser = await prisma.user.update({
+        where: {
+            id: userId,
+        },
+        data: {
+            name: name,
+        },
+    })
 
-  return {
-    name: updatedUser.name,
-  }
+    return {
+        name: updatedUser.name,
+    }
 }
 
 const changePassword = async (data: changePasswordType) => {
-  const { password, userId } = data
+    const { password, userId } = data
 
-  const existingUser = await prisma.user.findUnique({
-    where: {
-      id: userId,
-    },
-  })
+    const existingUser = await prisma.user.findUnique({
+        where: {
+            id: userId,
+        },
+    })
 
-  if (!existingUser) {
-    throw new Error('user does not exist')
-  }
+    if (!existingUser) {
+        throw new Error('user does not exist')
+    }
 
-  const hashPassword = await bcrypt.hash(password, 10)
+    const hashPassword = await bcrypt.hash(password, 10)
 
-  const updatedUser = await prisma.user.update({
-    where: {
-      id: userId,
-    },
-    data: {
-      password: hashPassword,
-    },
-  })
+    const updatedUser = await prisma.user.update({
+        where: {
+            id: userId,
+        },
+        data: {
+            password: hashPassword,
+        },
+    })
 
-  return {
-    name: updatedUser.name,
-  }
+    return {
+        name: updatedUser.name,
+    }
 }
 
 export const profileService = {
-  getProfile,
-  updateName,
-  changePassword,
+    getProfile,
+    updateName,
+    changePassword,
 }
