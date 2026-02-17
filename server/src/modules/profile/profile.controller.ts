16c588206df7a93370be968a4973e7bec8a29daf
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-02-17 22:29:11 +0530

    feat: profile

--- server/src/modules/profile/profile.controller.ts
+++ server/src/modules/profile/profile.controller.ts
@@ -4,115 +4,115 @@ import { success } from 'zod'
 import { changePasswordSchema, updateNameSchema } from './profile.schema'
 
 const getProfile = async (req: Request, res: Response) => {
-  const userId = req.userId
+    const userId = req.userId
 
-  if (!userId) {
-    return res.status(400).json({
-      success: false,
-      message: 'unauthorized',
-    })
-  }
+    if (!userId) {
+        return res.status(400).json({
+            success: false,
+            message: 'unauthorized',
+        })
+    }
 
-  try {
-    const result = await profileService.getProfile(userId)
-    return res.status(200).json({
-      success: true,
-      message: 'profile details',
-      data: result,
-    })
-  } catch (error: any) {
-    return res.status(400).json({
-      success: false,
-      message: 'user doesnot exist',
-      errors: error.message,
-    })
-  }
+    try {
+        const result = await profileService.getProfile(userId)
+        return res.status(200).json({
+            success: true,
+            message: 'profile details',
+            data: result,
+        })
+    } catch (error: any) {
+        return res.status(400).json({
+            success: false,
+            message: 'user doesnot exist',
+            errors: error.message,
+        })
+    }
 }
 
 const updateName = async (req: Request, res: Response) => {
-  const userId = req.userId as string | undefined
-  const parseData = updateNameSchema.safeParse(req.body)
+    const userId = req.userId as string | undefined
+    const parseData = updateNameSchema.safeParse(req.body)
 
-  if (!parseData.success) {
-    return res.status(400).json({
-      success: false,
-      message: 'invalid inputs',
-      errors: parseData.error.flatten(),
-    })
-  }
+    if (!parseData.success) {
+        return res.status(400).json({
+            success: false,
+            message: 'invalid inputs',
+            errors: parseData.error.flatten(),
+        })
+    }
 
-  if (!userId) {
-    return res.status(400).json({
-      success: false,
-      message: 'unauthorized',
-    })
-  }
+    if (!userId) {
+        return res.status(400).json({
+            success: false,
+            message: 'unauthorized',
+        })
+    }
 
-  const { name } = parseData.data
-  const typedUserId: string = userId
+    const { name } = parseData.data
+    const typedUserId: string = userId
 
-  try {
-    const result = await profileService.updateName({
-      userId: typedUserId,
-      name,
-    })
-    return res.status(200).json({
-      success: true,
-      message: 'name updated successfully',
-      data: result,
-    })
-  } catch (error: any) {
-    return res.status(400).json({
-      success: false,
-      message: 'failed to update name',
-      errors: error.message,
-    })
-  }
+    try {
+        const result = await profileService.updateName({
+            userId: typedUserId,
+            name,
+        })
+        return res.status(200).json({
+            success: true,
+            message: 'name updated successfully',
+            data: result,
+        })
+    } catch (error: any) {
+        return res.status(400).json({
+            success: false,
+            message: 'failed to update name',
+            errors: error.message,
+        })
+    }
 }
 
 const changePassword = async (req: Request, res: Response) => {
-  const userId = req.userId as string | undefined
-  const parseData = changePasswordSchema.safeParse(req.body)
+    const userId = req.userId as string | undefined
+    const parseData = changePasswordSchema.safeParse(req.body)
 
-  if (!parseData.success) {
-    return res.status(400).json({
-      success: false,
-      message: 'invalid inputs',
-      errors: parseData.error.flatten(),
-    })
-  }
+    if (!parseData.success) {
+        return res.status(400).json({
+            success: false,
+            message: 'invalid inputs',
+            errors: parseData.error.flatten(),
+        })
+    }
 
-  if (!userId) {
-    return res.status(400).json({
-      success: false,
-      message: 'unauthorized',
-    })
-  }
+    if (!userId) {
+        return res.status(400).json({
+            success: false,
+            message: 'unauthorized',
+        })
+    }
 
-  const { password } = parseData.data
-  const typedUserId: string = userId
+    const { password } = parseData.data
+    const typedUserId: string = userId
 
-  try {
-    const result = await profileService.changePassword({
-      userId: typedUserId,
-      password,
-    })
-    return res.status(200).json({
-      success: true,
-      message: 'password change successfully',
-      data: result,
-    })
-  } catch (error: any) {
-    return res.status(400).json({
-      success: false,
-      message: 'failed to change password',
-      errors: error.message,
-    })
-  }
+    try {
+        const result = await profileService.changePassword({
+            userId: typedUserId,
+            password,
+        })
+        return res.status(200).json({
+            success: true,
+            message: 'password change successfully',
+            data: result,
+        })
+    } catch (error: any) {
+        return res.status(400).json({
+            success: false,
+            message: 'failed to change password',
+            errors: error.message,
+        })
+    }
 }
 
 export const profileController = {
-  getProfile,
-  updateName,
-  changePassword,
+    getProfile,
+    updateName,
+    changePassword,
 }
--- server/src/modules/profile/profile.schema.ts
+++ server/src/modules/profile/profile.schema.ts
@@ -2,9 +2,9 @@ import { password } from 'bun'
 import { z } from 'zod'
 
 export const updateNameSchema = z.object({
-  name: z.string().min(3).max(20),
+    name: z.string().min(3).max(20),
 })
 
 export const changePasswordSchema = z.object({
-  password: z.string().min(6).max(20),
+    password: z.string().min(6).max(20),
 })
