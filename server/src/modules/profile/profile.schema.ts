e099c6678a792547abc945cc473140c250b731fd
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-02-17 17:19:01 +0530

    feat: add update name and change pass

---
+++ server/src/modules/profile/profile.schema.ts
@@ -0,0 +1,10 @@
+import { password } from 'bun'
+import { z } from 'zod'
+
+export const updateNameSchema = z.object({
+  name: z.string().min(3).max(20),
+})
+
+export const changePasswordSchema = z.object({
+  password: z.string().min(6).max(20),
+})
