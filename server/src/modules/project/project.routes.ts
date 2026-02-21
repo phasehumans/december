dbec01c78d9dd927b6c8e7a1236c563836543e4d
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-02-21 12:57:37 +0530

    project route struct

---
+++ server/src/modules/project/project.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { authMiddleware } from "../../middleware/auth.middleware";
+import { projectController } from "./project.controller";
+
+const projectRouter = Router()
+
+projectRouter.use(authMiddleware)
+projectRouter.get('/', projectController.getAllProjects)
+projectRouter.get('/:projectId', projectController.getProjectById)
+projectRouter.post('/', projectController.createProject)
+projectRouter.patch('/:projectId', projectController.updateProject)
+projectRouter.delete('/:projectId', projectController.deleteProject)
+
+export default projectRouter
