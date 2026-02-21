a9f8c96c3f584cb7c4c58726f09f513187d37931
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-02-21 14:27:09 +0530

    create and update project

--- server/src/modules/project/project.controller.ts
+++ server/src/modules/project/project.controller.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express"
 import { projectService } from "./project.service"
 import { success } from "zod"
+import { createProjectSchema, upadteProjectSchema } from "./project.schema"
 
 const getAllProjects = async (req: Request, res: Response) => {
     const userId = req.userId as string | undefined
@@ -64,11 +65,86 @@ const getProjectById = async (req: Request, res: Response) => {
 }
 
 const createProject = async (req: Request, res: Response) => {
+    const parseData = createProjectSchema.safeParse(req.body)
 
+    if(!parseData.success){
+        return res.status(400).json({
+            success: false,
+            message: "invalid inputs",
+            errors: parseData.error.flatten()
+        })
+    }
+
+    const userId = req.userId as string | undefined
+
+    if(!userId){
+        return res.status(400).json({
+            success: false,
+            message: "unauthorized"
+        })
+    }
+
+    try {
+        const projectInfo = parseData.data
+        const result = await projectService.createProject(projectInfo, userId)
+        return res.status(201).json({
+            success: true,
+            message: "project created successfully",
+            data: result
+        })
+    } catch (error: any) {
+        return res.status(500).json({
+            success: false,
+            message: "error while creating project",
+            errors: error.message
+        })
+    }
 }
 
 const updateProject = async (req: Request, res: Response) => {
+    const parseData = upadteProjectSchema.safeParse(req.body)
 
+    if(!parseData.success){
+        return res.status(400).json({
+            success: false,
+            message: "invalid inputs",
+            errors: parseData.error.flatten()
+        })
+    }
+
+    const userId = req.userId as string | undefined
+    const projectId = req.params.projectId as string | undefined
+
+
+    if(!projectId){
+        return res.status(400).json({
+            success: false,
+            message: "projectId is required"
+        })
+    }
+
+    if(!userId){
+        return res.status(400).json({
+            success: false,
+            message: "unauthorized"
+        })
+    }
+
+    try {
+        const updateProjectInfo = parseData.data
+        const result = await projectService.updateProject(updateProjectInfo, projectId, userId)
+        return res.status(200).json({
+            success: true,
+            message: "project details updated",
+            data: result
+        })
+    } catch (error: any) {
+        return res.status(500).json({
+            success: false,
+            message: "error while update project",
+            errors: error.message
+        })
+    }
 }
 
 const deleteProject = async (req: Request, res: Response) => {
