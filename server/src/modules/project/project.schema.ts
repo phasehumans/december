4b03d726d043dde264bbbd93bf7997027a09ce9d
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-03-03 10:08:44 +0530

    fix: update project route

--- server/src/modules/project/project.schema.ts
+++ server/src/modules/project/project.schema.ts
@@ -2,10 +2,11 @@ import { z } from 'zod'
 
 export const createProjectSchema = z.object({
     name: z.string(),
+    description: z.string().min(10).max(30).optional(),
     prompt: z.string(),
 })
 
 export const updateProjectSchema = z.object({
     rename: z.string().optional(),
-    starred: z.boolean().optional(),
+    isStarred: z.boolean().optional(),
 })
