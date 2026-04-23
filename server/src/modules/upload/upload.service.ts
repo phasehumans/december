093c2a70c27c73b44118f98edb5f035fe7f35c97
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-04-23 12:05:02 +0000

    add verify repo access and get access to private repo via tokens

--- server/src/modules/upload/upload.service.ts
+++ server/src/modules/upload/upload.service.ts
@@ -1,3 +1,6 @@
+import { prisma } from '../../config/db'
+import { parseGitHubRepoUrl, verifyGitHubRepoAccess } from './upload.utils'
+
 type UploadRepo = {
     userId: string
     repoURL: string
@@ -5,6 +8,32 @@ type UploadRepo = {
 
 const uploadRepo = async (data: UploadRepo) => {
     const { userId, repoURL } = data
+
+    const result = parseGitHubRepoUrl(repoURL)
+
+    if (result.ok == false) {
+        throw new Error(result.error)
+    }
+
+    const { owner, repo, normalizedUrl } = result
+
+    const user = await prisma.user.findUnique({
+        where: {
+            id: userId,
+        },
+    })
+
+    if (!user) {
+        throw new Error('user not found')
+    }
+
+    if (user.githubToken == undefined) {
+        throw new Error('access token not found')
+    }
+
+    const accestoken = user.githubToken as string
+
+    const res = await verifyGitHubRepoAccess(owner, repo, accestoken)
 }
 
 const uploadZip = async () => {}
