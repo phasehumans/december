6978bc4a562a8afc14a5bc12ac1d026653d00874
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-04-24 03:07:48 +0000

    fix: wsl and postman binding err

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,12 @@ import app from './app'
 const PORT = process.env.PORT || 4000
 const ENV = process.env.BUN_ENV
 
-app.listen(PORT, function () {
-    console.log(`server is listening on PORT ${PORT} in ${ENV} MODE`)
-})
+app.listen(
+    {
+        port: PORT,
+        hostname: '0.0.0.0',
+    },
+    () => {
+        console.log(`server is listening on PORT ${PORT} in ${ENV} MODE`)
+    }
+)
