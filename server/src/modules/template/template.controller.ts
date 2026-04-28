333591ab1315aa6b1f694113e8b5568303740644
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-04-28 16:24:52 +0000

    getall templates and by id

---
+++ server/src/modules/template/template.controller.ts
@@ -0,0 +1,105 @@
+import type { Request, Response } from 'express'
+import { templateService } from './template.service'
+import { success } from 'zod'
+
+const getAllTemplates = async (req: Request, res: Response) => {
+    const userId = req.userId as string | undefined
+
+    if (!userId) {
+        return res.status(400).json({
+            success: false,
+            message: 'unauthorized',
+        })
+    }
+
+    try {
+        const result = await templateService.getAllTemplates()
+        return res.status(200).json({
+            success: true,
+            message: 'templates fetched successfully',
+            data: result,
+        })
+    } catch (error: any) {
+        return res.status(500).json({
+            success: false,
+            error: error.message,
+        })
+    }
+}
+
+const getTemplateById = async (req: Request, res: Response) => {
+    const userId = req.userId as string | undefined
+    const templateId = req.params.templateId as string | undefined
+
+    if (!userId) {
+        return res.status(400).json({
+            success: true,
+            message: 'unauthorized',
+        })
+    }
+
+    if (!templateId) {
+        return res.status(400).json({
+            success: true,
+            message: 'templateId is required',
+        })
+    }
+
+    try {
+        const result = await templateService.getTemplateById(templateId)
+        return res.status(200).json({
+            success: true,
+            message: 'templates fetched successfully',
+            data: result,
+        })
+    } catch (error: any) {
+        return res.status(500).json({
+            success: false,
+            error: error.message,
+        })
+    }
+}
+
+const getTemplateByCategory = async (req: Request, res: Response) => {
+    const userId = req.userId as string | undefined
+    const category = req.params.category as string | undefined
+
+    if (!userId) {
+        return res.status(400).json({
+            success: false,
+            message: 'unauthorized',
+        })
+    }
+
+    if (!category) {
+        return res.status(400).json({
+            success: false,
+            message: 'category is needed',
+        })
+    }
+
+    // TODO >> check the category must from this 5-6 enums
+
+    try {
+        const result = await templateService.getTemplateByCategory(category)
+        return res.status(200).json({
+            success: true,
+            message: 'templates fetched successfully',
+            data: result,
+        })
+    } catch (error: any) {
+        return res.status(500).json({
+            success: false,
+            error: error.message,
+        })
+    }
+}
+
+const remixProject = async () => {}
+
+export const templateController = {
+    getAllTemplates,
+    getTemplateById,
+    getTemplateByCategory,
+    remixProject,
+}
