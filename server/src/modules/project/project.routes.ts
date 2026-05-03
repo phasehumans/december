b8fa7b3cd8c6f8168ac39d7027de5b19735e5dba
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-05-03 12:29:10 +0000

    fix project module and sep contracts

--- server/src/modules/project/project.routes.ts
+++ server/src/modules/project/project.routes.ts
@@ -1,19 +1,18 @@
 import { Router } from 'express'
-
 import { authMiddleware } from '../../middleware/auth.middleware'
-
 import { projectController } from './project.controller'
 
 const projectRouter = Router()
 
 projectRouter.use(authMiddleware)
 projectRouter.get('/', projectController.getAllProjects)
-projectRouter.get('/:projectId/download', projectController.downloadProjectVersion)
 projectRouter.get('/:projectId', projectController.getProjectById)
 projectRouter.post('/', projectController.createProject)
-projectRouter.patch('/:projectId', projectController.updateProject)
+projectRouter.patch('/:projectId', projectController.renameProject)
 projectRouter.delete('/:projectId', projectController.deleteProject)
 projectRouter.post('/:projectId/duplicate', projectController.duplicateProject)
+projectRouter.get('/:projectId/download', projectController.downloadProjectVersion)
 projectRouter.post('/:projectId/share', projectController.shareProjectAsTemplate)
+projectRouter.post('/:projectId/star', projectController.toggleStarProject)
 
 export default projectRouter
