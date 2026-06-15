1339417706e75c86599c390063ea9c08fbb8ce81
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-06-15 19:22:59 +0000

    refactore(december): adapt monorepo for december-cli

---
+++ apps/server/prisma/migrations/20260412102924_shared_project_as_template/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN     "isSharedAsTemplate" BOOLEAN NOT NULL DEFAULT false;
--- server/prisma/migrations/20260305092559_github_connect/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "githubConnected" BOOLEAN NOT NULL DEFAULT false;
