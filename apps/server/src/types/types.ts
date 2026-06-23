8a5f73879242e940f94fb1a025bc642aabbc7c12
chaitanya <dev.chaitanyasonawane@gmail.com>
2026-06-23 04:35:44 +0000

    remove docs

--- apps/server/src/types/types.ts
+++ apps/server/src/types/types.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-namespace */
-import type { TokenPayload } from '../modules/auth/auth.utils'
+import type { TokenPayload } from '../modules/auth/auth.types'
 
 declare global {
     namespace Express {
